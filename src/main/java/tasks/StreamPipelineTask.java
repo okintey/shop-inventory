eee9a89801ccab487d73df045edad663a30465fc
okintey <mariasapsai@gmail.com>
2024-12-04 13:07:50 +0200

    add stream pipeline task

---
+++ src/main/java/tasks/StreamPipelineTask.java
@@ -0,0 +1,11 @@
+package tasks;
+
+public class StreamPipelineTask {
+    public static void main(String[] args) {
+        // The pipeline should perform the following operations:
+        // 1. Filter out all the odd numbers.
+        // 2. Map the remaining numbers to their squares.
+        // 3. Limit the number of elements to 2.
+        // The list of integers is: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10.
+    }
+}
